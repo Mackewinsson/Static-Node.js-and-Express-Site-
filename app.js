774808c7555589a4d0b517bf87368d474097bbca
Mackewinsson <Mackewinsson@gmail.com>
2020-05-25 19:17:10 -0400

    Good for meets

--- app.js
+++ app.js
@@ -8,17 +8,56 @@ const { projects } = require('./data.json');
 app.set('view engine', 'pug');
 
 // MIDDLEWARES
-//   1. Static files
+
+     //   1. Static files
+
 app.use(express.static('public'));
 
+     //   1. Parse data
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // ROUTING
 
+app.get('/',(req, res, next)=>{
+     res.render('index', {projects});
+});
 
-app.all('/',(req, res, next)=>{
-     res.render('index');
-
+app.get('/about',(req, res, next)=>{
+     res.render('about');
 })
 
-console.log('App is running on port 3000')
-app.listen('3000');
+app.get('/project/:id',(req, res, next)=>{
+     const projectId = req.params.id;
+     const project = projects.find((element)=>{
+
+          if(element.id === projectId){
+               return element
+          };
+     });
+     console.log(project);
+     if(project){
+          res.render('project', project)
+     }else{
+          res.sendStatus(404)
+     };
+    
+});
+
+app.use((req, res, next) =>
+    {
+        const err = new Error('Not found')
+        err.status = 404;
+        next(err);
+    });
+
+app.use((err, req, res, next) =>
+    {
+        res.locals.error = err;
+        res.status(err.status);
+        res.render('error');
+    });
+
+const port = 3000;
+app.listen(port);
+console.log(`App is running on port ${port}`);
